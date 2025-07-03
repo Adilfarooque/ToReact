ec8981ecefdf1411395f780eec940729d0a40a94
adilfarooque <adilfarq11@gmail.com>
2025-07-03 10:59:09 +0400

    Updated with JSX

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 //-(npm install --save-dev @types/react @types/react-dom)
 // React.createElement => Object => Htmlelement(render)
-const heading = React.createElement("h1",{id:"heading"},"Into the React World 🚀");
+//const heading = React.createElement("h1",{id:"heading"},"Into the React World 🚀");
 
 //Throught Babel(parcel pkg)
 // JSX => React.createElement => ReactElement - JS Object => HTMLElement(render)
-const jsxheading = <h1 className="head" tabIndex="1">Assalaamu alikum people 🚀</h1>
-
+const heading = (<h1 id="heading" className="head">
+    Asssalamu alikum People 🚀</h1>)
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxheading)
+
+root.render(heading)
 
 
 
