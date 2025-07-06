d8b7e1def6c977cf1b70579ff5a46f5e4f20b84e
adilfarooque <adilfarq11@gmail.com>
2025-07-06 14:37:46 +0400

    Into the Component Composition

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//-(npm install --save-dev @types/react @types/react-dom)
-// React.createElement => Object => Htmlelement(render)
-//const heading = React.createElement("h1",{id:"heading"},"Into the React World 🚀");
 
-//Throught Babel(parcel pkg)
-// JSX => React.createElement => ReactElement - JS Object => HTMLElement(render)
-const heading = (<h1 id="heading" className="head">
-    Asssalamu alikum People 🚀</h1>)
+//React Element
+const Title = () => (
+<h1 className="head">THE HEAD TITLE 🚀</h1>
+);
+
+//React component
+/*------------------------------------------------*/
+//Class Based Components - OLD Way of writing code.
+//Fuctional Component - New way of writing code.
+
+//Component Composition
+const Heading = () => (
+    <div id="container">
+        <Title/>
+        <h1 id="heading"> Make some noice </h1>
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading)
-
-
-
-
+root.render(<Heading/>);
