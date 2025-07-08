03e205a6df61584478c52d0bff508eaf94fe0bab
adilfarooque <adilfarq11@gmail.com>
2025-07-09 00:52:44 +0530

    Updated the styles

--- app.js
+++ app.js
@@ -1,29 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+/*
+*Header
+* - Logo
+* - NavItems(rightside)
+*Body
+* - SearchBar
+* - RestrauntList
+*   - RestrauntCard
+*       - Image
+*       - Name
+*       - Rating
+*       - Cusines
+*Footer
+* - Copyright
+* - Links
+* - Address
+* - Contacts
+*/
+const Header = () => {
+    return(
+        <div className="header">
+            <div>
+                <img className="logo" src="https://w7.pngwing.com/pngs/664/210/png-transparent-uber-eats-muncheez-delivery-online-food-ordering-food-delivery-food-logo-uber-eats.png"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                    <li><img  className="cart_logo"src="https://img.icons8.com/?size=100&id=13014&format=png&color=000000"/></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
 
-const ele = <span>Is it ture</span>
 
-//React Element
-const title = (
-    <div id="container1">
-        {ele}
-        <h1>Make some noice......!</h1>
-    </div>
-);
-
-//React component
-/*------------------------------------------------*/
-//Class Based Components - OLD Way of writing code.
-//Fuctional Component - New way of writing code.
-
-//Component Composition
-const Heading = () =>(
-    <div id="container2">
-        <h2 id="heading">It a new thing</h2>
-    </div>
-);
+const AppLayout = () => {
+    return(
+        <div className="app">
+            <Header/>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<>{title}<Heading/></>);
+root.render(<AppLayout/>);
+
